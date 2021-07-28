fefb0a84e77862e62daba1db407882e8e9cab93d
sibinos <88077596+sibinos@users.noreply.github.com>
2021-07-28 12:29:23 +0530

    Add files via upload

---
+++ build/precache-manifest.8e32a04147c0fefd6183bf6ec04b48dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af63a6251e77e88db3469a362a1b64ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc450a3d611743979be",
+    "url": "/static/css/main.e97c723a.chunk.css"
+  },
+  {
+    "revision": "eeabd90213d1b9582bf1",
+    "url": "/static/js/2.7ac64ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ac64ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc450a3d611743979be",
+    "url": "/static/js/main.8986fdf8.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
